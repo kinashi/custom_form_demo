c8024e19ecb7916834d78c2c61f0c9b6c043581e
Yuta Kinashi <y.nashiki@gmail.com>
2019-05-26 18:39:09 +0900

    Add Routing

---
+++ app/controllers/custom_forms_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class CustomFormsController < ApplicationController
+  def edit; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :teams, only: %i[show] do
+    resources :custom_forms, only: %i[edit update]
+  end
 end
